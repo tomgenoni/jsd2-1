6da6fa98c671c903bbc1a842f2cc1db858863272
timm <timmmichaud@gmail.com>
2016-07-12 13:02:31 -0700

    Student profile pages

---
+++ functions/js/functions.js
@@ -0,0 +1,13 @@
+console.log("functions")
+
+function subtract(x, y) {
+	var difference = x - y;
+	return difference;
+}
+
+function multiply(x, y) {
+
+	// multiple two numbers, store result in product
+	var product = x * y;
+	return product;
+}
