7a3d06d57cd8073bbfad4efe020bb1cd8cf8718a
timm <timmmichaud@gmail.com>
2016-07-12 19:30:41 -0700

    homework

---
+++ homework/01-formulas/materials/formulas/js/formulas.js
@@ -0,0 +1,8 @@
+
+// Instructions for your homework
+// //////////////////////////////////////////////////////////////////
+// 1.   Here is where your functions should be defined
+// 2.	 What should you name your functions?  Hint:  check the console to see which functions are already being called.  Are they all "defined?"  If not yet defined... then define them here!
+// 3.	 Be sure to link up this file in your HTML doc
+/////////////////////////////////////////////////////////////////////
+
